1f41c256c0dbb569924a6f5a67e6e0ef84fdff45
tomato <tengokukotobaaitohikari358@gmail.com>
2026-08-19 20:48:37 +0900

    correct code commit

--- UserController.php
+++ UserController.php
@@ -4,21 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
+        // $users = User::all(); ←コードレビューより修正
+        $users = User::paginate(20)->get();
         return view('users.index', ['users' => $users]);
     }
 
     public function store(Request $request)
     {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+        ]);
+
         $user = new User;
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = $request->password;
+        $user->name = $validated['name'],
+        $user->email = $validated['email'],
+        $user->password = Hash::make($validated['password']),
         $user->save();
 
         return redirect('/users');
